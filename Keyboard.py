a6f07642bd2852c4eb9c8c99c28b51a94269ddcb
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-09-29 13:10:05 +0200

    Har fixat så att Keyboard.py kan visa en layout

--- Keyboard.py
+++ Keyboard.py
@@ -10,35 +10,40 @@ keyboard_showcase.title("Keyboard Layout")
 keyboard_showcase ['bg']='powder blue'
 keyboard_showcase.resizable (0,0)
 
-labell = Label (keyboard_showcase, text="E", font =('arial', 30, 'bold'),
+labell = Label (keyboard_showcase, text="", font =('arial', 30, 'bold'),
                 bg ='powder blue', fg="#000000") .grid(row = 0, columnspan = 40)
-entry = Text(keyboard_showcase, width=138, font =('arial', 10, 'bold')) .grid(row = 1, columnspan = 40)
-
-keyboard_layout = [
+#entry = Text(keyboard_showcase, width=138, font =('arial', 10, 'bold')) .grid(row = 1, columnspan = 40)
+#import random
+Optimized_layout = [
     'q','w','e','r','t','y','u','i','o','p','{[',']}',
     'a','s','d','f','g','h','j','k','l',':;',"@'",'#',
     '|','z','x','c','v','b','n','m','<,','.>','?/'
 ]
-varRow = 3
+#random.shuffle(Optimized_layout)
+
+
+varRow = 2
 varColumn = 0
 
-for button in keyboard_layout:
+for button in Optimized_layout:
     command = lambda x= button: select(x)
-    if button != " Space":
-        tkinter.Button(keyboard_showcase, text= button,
-                        command = command) .grid(row=varRow, column = varColumn)
 
     if button != " Space":
         tkinter.Button(keyboard_showcase, text= button,
                         command = command) .grid(row=varRow, column = varColumn)
 
+    if button != " Space":
+        tkinter.Button(keyboard_showcase, text= button,
+                        command = command) .grid(row=varRow, column = varColumn)
     varColumn +=1
-    if varColumn > 15 and varRow == 2:
+    if varColumn > 11 and varRow == 2:
         varColumn = 0
         varRow +=1
 
-    if varColumn > 15 and varRow == 3:
+    if varColumn > 11 and varRow == 3:
         varColumn = 0
         varRow +=1
+
+
 
 keyboard_showcase.mainloop()
--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 
-"""
-import random
+
 
 #Hämtar Counter
 from collections import Counter
@@ -11,15 +10,16 @@ Content_All = content.lower()
 
 print(Counter(Content_All))
 
-"""
+
 #Värden för tangenter, 0, 1, 1.038, 1.118, 2.138, 1.247, 1.605, 1.803, 2.661, 2.015
+#stagger 1/4 upper, 1/2 lower
 
 #import random
 
 
 target_chars = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{[', ']}',
                 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':;', "@'", '#',
-                '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<,', '.>',]
+                '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<,', '.>','?']
 
 #random.shuffle(target_chars)
 
@@ -27,7 +27,7 @@ pos_value = {
     frozenset([0, 1, 2, 3, 7, 8, 9]): 1.032,
     frozenset([4, 10]): 1.247,
     frozenset([5]): 1.605,
-    frozenset([11]): 0,  # You need to fill in the value for this key.
+    frozenset([11]): 0,  # need value
     frozenset([12, 13, 14, 15, 18, 19, 20, 21]): 0,
     frozenset([16,17,22]): 1,
     frozenset([23]): 2,
@@ -44,4 +44,6 @@ for i, char in enumerate(target_chars):
 # Print the result
 for char, effort in char_to_effort.items():
     print(f"Character '{char}' has effort {effort}")
+
+
 
