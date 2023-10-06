27626b47f2e91fbe3478341246b8fac7aa49acb4
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-10-06 10:53:25 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ print(Counter(Content_All))
 
 
 target_chars = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{[', ']}',
-                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':;', "@'", '#',
+                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':;', "@'", '~#',
                 '|\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<,', '.>','?']
 
 #random.shuffle(target_chars)
